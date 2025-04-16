ac78018975a8f8876ee3c6da25ace91529d39085
phil <philip@fortunehomesteam.com>
2025-04-17 00:38:54 +0800

    Fix images for testimonees

--- src/data/testimonies.jsx
+++ src/data/testimonies.jsx
@@ -1,14 +1,17 @@
+import nhan from "../assets/testimonies/nhan.png"
+import wes from "../assets/testimonies/wes.png"
+
 
 const testimonies = [
     {
         message: "Fortune Tech Solutions helped me create a site for my business with multiple forms and functions tied to our CRM.  They were professional in design, layout, and integration with all of my tools.  Thank you so much for your hard work",
-        image: "/src/assets/testimonies/nhan.png",
+        image: nhan,
         name: "Nhan Lam",
         position: "Coach, CEO at FortuneHomesTeam"
     },
     {
         message: "I want to extend my heartfelt thanks to Fortune Tech Solutions for their outstanding work on my company's website. Their expertise, attention to detail, and commitment to delivering a high-quality solution truly exceeded my expectations. The entire process was seamless, and their team was always available to address any questions or concerns. Thanks to their efforts, our website is now more functional, visually appealing, and user-friendly. I highly recommend Fortune Tech Solutions to anyone looking for top-notch IT solutions!",
-        image: "/src/assets/testimonies/wes.png",
+        image: wes,
         name: "Wes",
         position: "Coach, CEO at CashOfferSolid"
     },
